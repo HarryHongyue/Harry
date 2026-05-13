dab4556536dab101612923ea79e103760eb1b739
Hongyue Ji (Harry) <HarryHongyue@Gmail.com>
2026-05-13 12:09:40 +0200

    Update project preview card icon size and social logo

--- src/components/common/SocialLogo.tsx
+++ src/components/common/SocialLogo.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import emailLogo from '../../assets/images/外部应用logo/邮箱.svg';
-import signalLogo from '../../assets/images/外部应用logo/Signal-Logo.svg';
-import telegramLogo from '../../assets/images/外部应用logo/Telegram-Logo.svg';
+import signalLogo from '../../assets/images/外部应用logo/Signal-Logo-Ultramarine_(2024).svg.png';
+import telegramLogo from '../../assets/images/外部应用logo/Telegram (Telegram).png';
 import whatsappLogo from '../../assets/images/外部应用logo/WhatsApp.png';
 import xLogo from '../../assets/images/外部应用logo/X-Logo.svg';
-import xchatLogo from '../../assets/images/外部应用logo/Xchat-Logo.svg';
+import xchatLogo from '../../assets/images/外部应用logo/Xchat-Logo.png';
 
 type SocialLogoName = 'email' | 'whatsapp' | 'telegram' | 'xchat' | 'x' | 'signal';
 
